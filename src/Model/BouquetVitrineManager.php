ab5a8abe8f77d853e5911c201bde46399688dee2
Maxime <maximej.becker@gmail.com>
2021-04-22 15:35:19 +0200

    Le stock

---
+++ src/Model/BouquetVitrineManager.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Model;
+
+class BouquetVitrineManager extends AbstractManager
+{
+
+    public const TABLE = 'bouquetVitrine';
+    public const TABLE_2 = 'stock_bouquetVitrine';
+    public function insert(array $stock): int
+    {
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (name, price ) VALUES (:name, :price)");
+        $statement->bindValue('name', $stock['name'], \PDO::PARAM_STR);
+        $statement->bindValue('price', $stock['price'], \PDO::PARAM_INT);
+
+        $statement->execute();
+        return (int)$this->pdo->lastInsertId();
+    }
+
+    public function insertStockBouquetVitrine(array $commande): void
+    {
+        $query = ("INSERT INTO " . self::TABLE_2 . " (stock_id, command_id) 
+                    VALUES (:stock_id, :command_id)");
+        $req = $this->pdo->prepare($query);
+        $req->bindValue('stock_id', $commande['stock_id'], \PDO::PARAM_INT);
+        $req->bindValue('command_id', $commande['command_id'], \PDO::PARAM_INT);
+
+        $req->execute();
+    }
+
+
+    public function update(array $stock): bool
+    {
+        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `name` = :name , `price` = :price
+         WHERE id=:id");
+        $statement->bindValue('id', $stock['id'], \PDO::PARAM_INT);
+        $statement->bindValue('name', $stock['name'], \PDO::PARAM_STR);
+        $statement->bindValue('price', $stock['price'], \PDO::PARAM_INT);
+
+        return  $statement->execute();
+    }
+}
