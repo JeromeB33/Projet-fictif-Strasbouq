1cfa38b3b67f5705fd33f8928d2a7d2312cd562e
Mélissa Kintz <kntzmelissa@gmail.com>
2021-04-29 10:06:12 +0200

    add pages choisi bouquet and compose bouquet

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -48,4 +48,20 @@ class HomeController extends AbstractController
     {
         return $this->twig->render('Home/signIn.html.twig');
     }
+
+    /*
+     * display page compose ton bouquet
+     */
+    public function composeBouquet(): string
+    {
+        return $this->twig->render('Home/compose.html.twig');
+    }
+
+    /*
+     * display page choisi ton bouquet
+     */
+    public function choisiBouquet(): string
+    {
+        return $this->twig->render('Home/choisi.html.twig');
+    }
 }
